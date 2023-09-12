c4c62efdb40faf696091e44a6ea6c5d8c94ecce7
nabin-bh <nabinbhandari104@gmail.com>
2023-09-12 18:58:21 +0545

    redis getting data

--- main.go
+++ main.go
@@ -1,44 +1,42 @@
 package main
 
-
-import ( 
-    "context"
-    "fmt"
-    "github.com/go-redis/redis/v8"
+import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 )
 
-
-func main(){
+func main() {
 	r := gin.Default()
 	r.GET("/create-redis", Create)
 	r.GET("/get-redis", Get)
 	r.GET("/delete-redis", Delete)
 	r.GET("/create-list-redis", CreateList)
 	r.GET("/create-set-redis", CreateSet)
-  	r.Run()
+	r.Run()
 }
 
-func RedisConnection()(*redis.Client, context.Context){
+func RedisConnection() (*redis.Client, context.Context) {
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379", // Default Redis address
-		Password: "",              // No password by default
-		DB:       0,               // Default Redis database
+		Password: "",               // No password by default
+		DB:       0,                // Default Redis database
 	})
 
 	ctx := context.Background()
 
 	_, err := client.Ping(ctx).Result()
 	if err != nil {
-		fmt.Println("Error connecting to Redis:", err) 
+		fmt.Println("Error connecting to Redis:", err)
 	}
 
 	return client, ctx
 }
 
-func Create(c *gin.Context){
+func Create(c *gin.Context) {
 	client, ctx := RedisConnection()
-	 
+
 	errs := client.Set(ctx, "key", "value 22", 0).Err()
 	if errs != nil {
 		fmt.Println("Error setting key:", errs)
@@ -46,10 +44,10 @@ func Create(c *gin.Context){
 		fmt.Println("Key set successfully")
 	}
 	c.JSON(200, "redis: data inserted successfully")
-	return 
+	return
 }
 
-func Get(c *gin.Context){
+func Get(c *gin.Context) {
 	client, ctx := RedisConnection()
 	value, err := client.Get(ctx, "key").Result()
 	if err != nil {
@@ -57,11 +55,11 @@ func Get(c *gin.Context){
 	} else {
 		fmt.Println("Value:", value)
 	}
-	c.JSON(200, "redis: get data 'key': "+ value)
-	return 
+	c.JSON(200, "redis: getting data 'key': "+value)
+	return
 }
 
-func Delete(c *gin.Context){
+func Delete(c *gin.Context) {
 	client, ctx := RedisConnection()
 	err := client.Del(ctx, "key").Err()
 	if err != nil {
@@ -71,10 +69,10 @@ func Delete(c *gin.Context){
 	}
 
 	c.JSON(200, "redis: data deleted !!")
-	return 
+	return
 }
 
-func CreateList(c *gin.Context){
+func CreateList(c *gin.Context) {
 	client, ctx := RedisConnection()
 	err := client.RPush(ctx, "myList", "item1", "item2", "item3").Err()
 	if err != nil {
@@ -82,11 +80,10 @@ func CreateList(c *gin.Context){
 	}
 
 	c.JSON(200, "redis: list data inserted success !!")
-	return 
+	return
 }
 
-
-func CreateSet(c *gin.Context){
+func CreateSet(c *gin.Context) {
 	client, ctx := RedisConnection()
 	err := client.SAdd(ctx, "mySet", "member1", "member2", "member3").Err()
 	if err != nil {
@@ -94,5 +91,5 @@ func CreateSet(c *gin.Context){
 	}
 
 	c.JSON(200, "redis: set data inserted success !!")
-	return 
+	return
 }
